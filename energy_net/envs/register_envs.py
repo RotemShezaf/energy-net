b8b64aad170e2b8430439202248952dc52ef48e7
RotemShezaf <71847237+RotemShezaf@users.noreply.github.com>
2025-07-15 18:20:56 +0300

    Update register_envs.py

--- energy_net/envs/register_envs.py
+++ energy_net/envs/register_envs.py
@@ -6,7 +6,7 @@ from gymnasium.envs.registration import register
 print("Registering EnergyNetEnv-v0")
 register(
     id='EnergyNetEnv-v0',
-    entry_point='energy_net.env.energy_net_v0:EnergyNetV0',
+    entry_point='energy_net.envs.energy_net_v0:EnergyNetV0',
     # Optional parameters:
     # max_episode_steps=1000,   
     # reward_threshold=100.0,
